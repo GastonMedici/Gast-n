be8b3dfc687e28f207e97adb5da8f17fb2c1632b
GastonMedici <127124483+GastonMedici@users.noreply.github.com>
2023-07-07 17:49:49 -0300

    Update producto_vendido.js 7

--- js/producto_vendido.js
+++ js/producto_vendido.js
@@ -11,7 +11,7 @@ createApp({
             stock: 0,
             precio: 0,
             url: 'http://gasmed.pythonanywhere.com/productos/' + id,
-        }
+        };
     },
     methods: {
         fetchData(url) {
